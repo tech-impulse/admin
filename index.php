f03c5046acfc72ff54a27b7dd3e72509860589ae
Alain Cidrera <alain.cidrera@tech-impulse.com>
2015-12-18 11:22:54 +0100

    Ultima versión

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
     </div>
 </body>
     <?php
-echo 
+
 if (isset($_GET['password']) && isset($_GET['user'])) {
     echo "<script> $('#emailLogin').val(".$_GET['user']."); $('#passwordLogin').val(".$_GET['password'].");login();</script>";
 }
